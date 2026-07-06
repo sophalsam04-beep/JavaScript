5342f9b25472ea23277397cece6c8357ca20eb57
Phal Developer <sophalsam04@gmail.com>
2026-07-06 23:06:58 +0700

    Create 10_math.js

---
+++ 10_math.js
@@ -0,0 +1,39 @@
+// math in Javascripts
+
+let a=10;
+let b=2;
+let result = Math.round(4,5);
+console.log(result);
+let result_1 = Math.min(5,100);
+console.log(result_1);
+let result_2 = Math.max(5,100);
+console.log(result_2);
+
+console.log(Math.PI); // PI = 3.14
+console.log(Math.sqrt(64));
+console.log(Math.sqrt(8));
+console.log(Math.abs(-10));  // absolute values
+console.log(Math.abs(10));
+console.log(Math.abs(-100));
+
+console.log(Math.pow(3,3));
+console.log(Math.E);   // Natural Logirthm
+console.log(Math.round(4.6));
+console.log(Math.round(10));
+console.log(Math.round(5,100));
+console.log(Math.ceil(4,1));
+console.log(Math.ceil(5,3));
+
+console.log(Math.ceil(5,40));
+console.log(Math.ceil(100,34));
+
+
+console.log(Math.trunc(4.5));
+console.log(Math.trunc(3.100));
+
+function Mul(a,b){
+    return (a*b);
+}
+
+console.log(Mul(5,3));
+
