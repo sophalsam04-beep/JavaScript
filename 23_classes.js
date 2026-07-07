43c4f523d6bb3c08ef82182eacc8f23fd6ffc59e
Phal Developer <sophalsam04@gmail.com>
2026-07-07 21:22:06 +0700

    Create 23_classes.js

---
+++ 23_classes.js
@@ -0,0 +1,15 @@
+// classes
+
+    class Student{
+        // store data
+        constructor(name){
+            this.name = name;
+        }
+
+        display(){
+            console.log(this.name);
+        }
+    }
+
+        const students = new Student("Un virak");
+            students.display();
