afee9f7ca253424a3adbe8d89c3d9de10d927874
Phal Developer <sophalsam04@gmail.com>
2026-07-07 21:24:53 +0700

    Create 24_loop_of.js

---
+++ 24_loop_of.js
@@ -0,0 +1,10 @@
+  // Loops of
+const numbers = [1,2,3,4,5,6,7,8,9,10];
+for (const num of numbers) {
+    console.log(num);
+}
+
+const value = [43,25,33,42,22,4];
+for (const data of value) {
+    console.log(data);
+}
